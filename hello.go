d0fc49ecb688274011884ab4dbc6c2dcd4835cf9
Kyriezhouzhou <93136931+Kyriezhouzhou@users.noreply.github.com>
2021-11-23 10:15:17 +0800

    Update hello.go

--- hello.go
+++ hello.go
@@ -6,8 +6,8 @@ func main() {
  r := gin.Default()
  r.GET("/ping", func(c *gin.Context) {
      c.JSON(200, gin.H{
-         "message": "hello zijie 20211022 and test 55555 站内信通知44443333 !!!",
+         "message": "hello huoshan 20211022 and test 55555 站内信通知44443333 !!!",
      })
  })
- r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+ r.Run() // listen and serve on 0.0.0.0:8000 (for windows "localhost:8080")
 }
